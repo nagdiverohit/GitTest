4d69c95e68e08190e437d91637e3d6ce31ae9fc6
Rohit <nagdiverohit7692@gmail.com>
2023-03-20 23:11:14 +0530

    Added multiply

---
+++ multi[ly.java
@@ -0,0 +1,12 @@
+public class multiply
+{
+public static void main(String args[])
+{
+int x = 4;
+int y = 5;
+int sum = x + y;
+System.out.println("The sum of x and y is: " +sum);
+System.out.println("The sum of x and y is: " +sum);
+System.out.println("The sum of x and y is: " +sum);
+}
+}
